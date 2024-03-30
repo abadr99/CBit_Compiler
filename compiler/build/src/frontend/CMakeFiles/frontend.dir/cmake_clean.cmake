d7f79ac37c268acc4d7904ffb396e3687edebe5f
abadr99 <badr@Ubuntu.myguest.virtualbox.org>
2024-03-30 02:18:15 +0200

    Initial Support For Build System

---
+++ compiler/build/src/frontend/CMakeFiles/frontend.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/frontend.dir/lexer.cpp.o"
+  "libfrontend.a"
+  "libfrontend.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/frontend.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
