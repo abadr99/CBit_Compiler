39b9e24f07c4e53096fd4d7357feea31b7ad29a3
abadr99 <badr@Ubuntu.myguest.virtualbox.org>
2024-04-27 19:22:58 +0300

    dbg

--- scripts/github/buildCompiler.sh
+++ scripts/github/buildCompiler.sh
@@ -1,4 +1,6 @@
 # !/bin/sh
 # This script intented to be run from ${{ github.workspace }}
-cmake -B ./compiler/build 
-make -C ./compiler/build -j
+cd ./compiler
+cmake -B ./build 
+make  -C ./build -j
+cd -
