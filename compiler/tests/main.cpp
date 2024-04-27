4bb1fa7c2453bf7b00fb62a388c30b610e3e786c
abadr99 <badr@Ubuntu.myguest.virtualbox.org>
2024-04-28 01:06:00 +0300

    Add test to build system

---
+++ compiler/tests/main.cpp
@@ -0,0 +1,17 @@
+/**
+ * @file main.cpp
+ * @author abadr99 (abdelrhmanezzbadr@outlook.com)
+ * @brief 
+ * @version 0.1
+ * @date 2024-04-27
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
+#include <gtest/gtest.h>
+
+int main(int argc, char **argv) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
