15599aaa9082af1967257da99134fc109f35492a
abadr99 <badr@Ubuntu.myguest.virtualbox.org>
2024-04-27 18:54:38 +0300

    Fix some linter issues

--- compiler/dev/src/common/OptionParser.cpp
+++ compiler/dev/src/common/OptionParser.cpp
@@ -1,3 +1,14 @@
+/**
+ * @file OptionParser.cpp
+ * @author abadr99 (abdelrhmanezzbadr@outlook.com)
+ * @brief 
+ * @version 0.1
+ * @date 2024-04-27
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
 #include "common/OptionParser.hpp"
 
 using namespace cbit::options;
