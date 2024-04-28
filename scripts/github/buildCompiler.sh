6033650bc543e97477bdbd006a93f1a36bbfcb90
abadr99 <badr@Ubuntu.myguest.virtualbox.org>
2024-04-28 22:07:17 +0300

    [CI]: Fix ci after new build system

--- scripts/github/buildCompiler.sh
+++ scripts/github/buildCompiler.sh
@@ -1,14 +1,14 @@
 # !/bin/sh
 # This script intented to be run from ${{ github.workspace }}
-cd ./compiler/dev
-cmake -B ../build 
+cd ./compiler
+cmake -B ./build 
 
 if [ $? -gt 0 ]
 then 
     exit 1
 fi
 
-make  -C ../build -j
+make  -C ./build -j
 
 if [ $? -gt 0 ]
 then 
