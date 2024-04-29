c4d328e6fd42cfb1e0d2e21633b47fc855a9ff76
abadr99 <badr@Ubuntu.myguest.virtualbox.org>
2024-04-29 21:24:50 +0300

    [TEST]: Add new tests for option parser

--- compiler/tests/utils/option-parser.cpp
+++ compiler/tests/utils/option-parser.cpp
@@ -1,7 +1,19 @@
 #include <gtest/gtest.h>
+#include "common/CompilerOptions.hpp"
+#include "common/OptionParser.hpp"
 
-TEST(OptionParser, test1) {
-    EXPECT_EQ(1,1);
-    EXPECT_EQ(1,2);
+using namespace cbit::options;
+
+TEST(OptionParser, files) {
+    // Set argc and argv as command arguments by assuming the command is
+    // cbit path/to/file1.c path/to/file2.c
+    int argc = 3;
+    const char* argv[] = {"cbit", "path/to/file1.c", "path/to/file2.c"};
+    
+    OptionParser op(argc,const_cast<char**>(argv));
+    auto& opts = op.GetOptions();
+
+    EXPECT_STREQ(opts.GetCompiledFiles()[0].c_str(), "path/to/file1.c");
+    EXPECT_STREQ(opts.GetCompiledFiles()[1].c_str(), "path/to/file2.c");
 }
 
